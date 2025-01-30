61266ea0fe52b83743101605850aa523afe95561
Vaishnavi Kumari <132896197+VaishnaviiKumari@users.noreply.github.com>
2025-01-30 10:27:29 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,32 @@
+import Login from "./pages/login/Login";
+//import Signup from "./pages/signup/Signup";
+
+function App() {
+  return (
+    <div>
+      <Login />
+     
+    </div>
+  );
+}
+
+export default App;
+
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Login from "./pages/login/Login";
+// import Signup from "./pages/signup/Signup";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Login />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/signup" element={<Signup />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
