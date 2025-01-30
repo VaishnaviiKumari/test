60b75caa55a7fc9b5584e55c0ee3031d027c7a56
Vaishnavi Kumari <132896197+VaishnaviiKumari@users.noreply.github.com>
2025-01-30 10:26:28 +0530

    Login.js

---
+++ Login.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+// import "./Login.css";
+import "./Login.css";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  const validateForm = (e) => {
+    e.preventDefault();
+
+    if (email.length < 9) {
+      setEmailError(true);
+      return;
+    } else {
+      setEmailError(false);
+    }
+
+    if (password.length < 6) {
+      setPasswordError(true);
+      return;
+    } else {
+      setPasswordError(false);
+    }
+
+    alert("Login Successful");
+  };
+
+  return (
+    <div className="container">
+      <h1 className="label">User Login</h1>
+      <form className="login_form" onSubmit={validateForm}>
+        <div className="font">Email or Phone</div>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {emailError && <div className="error">Please fill up your Email or Phone</div>}
+
+        <div className="font font2">Password</div>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {passwordError && <div className="error">Please fill up your Password</div>}
+
+        <button type="submit">Login</button>
+
+        <div>
+          <a href="./pages/signup/Signup.js">
+            <p>Create an account</p>
+          </a>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
